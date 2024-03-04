ee74e7cd0a08b8858048974f4daa80a8ec3fa23a
13wendy <161635340+13wendy@users.noreply.github.com>
2024-03-04 15:56:43 -0800

    creado por wendy

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+$animales=[
+"animales" => "caballo",
+"raza" => "frison",
+"edad" => 2,
+"estatura" => 1.75,
+"peso" => 1000,
+"velocidad" => 70.76,
+"color" => "negro"
+"huesos" => 205
+"vertebras" => 51,
+"pesuñas" => 4,
+"color de ojos" => "cafes"
+"valor" => 47.900,
+"patas" =>4,
+"esperanza de vida" =>25
+"donde se encuentra" => "holanda"
+"cantidad mundial" => " 50.000.000,
+"genero" => "hembra",
+"familia" => "equidos",
+"gestacion" => 10,
+"nombre del caballo" => "atenea",
+"vista" => "panoramicos",
+];
+
+<?php  
+$animales2=[
+"animal" => "perro ", 
+"nombre del perro" => "muñeca"
+"raza" => "pit bull", 
+"peso" => 25, 
+"años" => 3, 
+"familia" => "canina",
+"olfato" => "agudo", 
+"color" => "gris",
+"cantidad de huesos" => 320,
+ "tamaño" => "grande", 
+"patas" => 4, 
+"genero" => "hembra",
+"ojos " => 2, 
+"color de ojos" => "cafes" , 
+" tiempo de vida" => 16 , 
+"alimentacion" => " croquetas",
+"tiempo de gestacion" => "68 dias", 
+"estatura" => 110,
+" valor " => 3000
+"tipos de pit bull" => 4,
+];
